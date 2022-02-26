cea9a813afed814594314a26f628fdf90211b94f
Timon Link <timon.link@gmail.com>
2022-02-26 14:28:15 +0100

    Add user document

---
+++ src/main/java/link/timon/tutorial/sercurerest/notes/domain/User.java
@@ -0,0 +1,25 @@
+package link.timon.tutorial.sercurerest.notes.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * This model represents a user and functions
+ * as mongo db document.
+ * 
+ * @author Timon
+ */
+@Document
+@Data
+public class User {
+    
+    @Id
+    private String id;
+    
+    private String name;
+    
+    private String email;
+    
+    private String password;
+}
