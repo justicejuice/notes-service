f12af9d886d2343edc27dbb6f8f402c22259698e
Timon Link <timon.link@gmail.com>
2022-03-15 10:22:36 +0100

    auditable

---
+++ src/main/java/link/timon/tutorial/securerest/notes/config/MongoDataConfig.java
@@ -0,0 +1,15 @@
+package link.timon.tutorial.securerest.notes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+/**
+ * Configuration for Mongo Data.
+ *
+ * @author Timon Link
+ */
+@Configuration
+@EnableMongoAuditing
+public class MongoDataConfig {
+
+}
